6119ac785f742dcaa35dcb51441060b9ff4625fe
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2026-04-24 15:11:34 +0530

    Implement User model with schema validation

---
+++ src/models/user.model.ts
@@ -0,0 +1,21 @@
+import mongoose, { Document, Schema } from "mongoose"
+
+export interface IUSER extends Document {
+  _id: mongoose.Types.ObjectId
+  firstname?: string
+  lastname?: string
+  email: string
+  password: string
+}
+
+const userSchema = new Schema<IUSER>(
+    {
+        firstname: { type: String, required: true },
+        lastname: { type: String, required: true },
+        email: { type: String, unique: true, lowercase: true, required: true },
+        password: { type: String, required: true },
+    },
+    { timestamps: true }
+)
+
+export const User = mongoose.model<IUSER>("User", userSchema)
